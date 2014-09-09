00daedcf9a89cc175b49202c9b0ff84deaf4d58f
Hsue <hhsue@sfo1m-c02g282x.etrade.com>
2014-09-09 13:39:12 -0700

    adding shell script

---
+++ createSymlinksDotfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+files=".bashrc .vimrc .vimogen_repos"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "1. Removing old $olddir, and creating new $olddir for backup. ~"
+rm -r $olddir
+mkdir -p $olddir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "2. Moving any existing dotfiles from ~ to $olddir"
+for file in $files; do
+    mv ~/$file ~/dotfiles_old/
+    echo "$file: Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
+echo "...done"
+
