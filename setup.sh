656ba25f16a79ebb84703d99ec12bf5a7750c456
Henry Hsue <hhenry@uber.com>
2017-03-18 16:10:30 -0700

    remove old stuff

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-brew install python 
-brew install vim --with-python --with-ruby --with-perl
-sudo cp /usr/local/bin/vim /usr/bin/vim
-
